7c30af4f695134af3be189ff908ca6a6fc96e83c
Konrad-Jalowy <sitesystem@wp.pl>
2023-02-02 10:21:50 +0100

    very simple counter created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleSubtractOne = () => {
+    setCount(count - 1);
+ }
+ const handleAddOne = () => {
+  setCount(count + 1);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>{count}</p>
+    <button onClick={handleSubtractOne}>-1</button>
+    <button onClick={handleAddOne}>+1</button>
+    </>
+    
   );
 }
 
